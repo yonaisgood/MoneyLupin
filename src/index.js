dbb18463d3a354cad3e11f28d1561d8bfa997d30
yonainthefish <yonaisgood@gmail.com>
2023-08-15 23:24:50 +0900

    Chore: 초기세팅

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 // import ReactDOM from 'react-dom'; //구버전
-import { createRoot } from "react-dom/client";
+import {createRoot} from "react-dom/client";
 import App from "./App";
+import "normalize.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
