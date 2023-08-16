3962d05f0b86250f8153801aa5f575dfe183e184
Kim Hayeon <k.hayeon00@gmail.com>
2023-08-16 19:51:29 +0900

    Design: 홈 페이지 작업중

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -67,6 +67,11 @@ const GlobalStyle = createGlobalStyle`
   h1, h2, h3, h4, h5, h6 {
     margin: 0;
   }
+
+  ul {
+    margin: 0;
+    padding: 0;
+  }
 `;
 
 export default GlobalStyle;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,153 @@
 import Header from '../components/Header';
+import { styled } from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import banner1 from '../assets/images/banner1.png';
+import banner2 from '../assets/images/banner2.png';
+import banner3 from '../assets/images/banner3.png';
+
+import best1 from '../assets/images/best/1.png';
+import best2 from '../assets/images/best/2.png';
+import best3 from '../assets/images/best/3.png';
+import best4 from '../assets/images/best/4.png';
 
 const Home = () => {
   return (
     <>
-      <Header></Header>
+      <Header />
+      <StyledMain>
+        <section className="banners">
+          <h2 className="a11y-hidden">메인 배너</h2>
+          <ul>
+            <li>
+              <Link>
+                <img src={banner1} alt="" />
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <img src={banner2} alt="" />
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <img src={banner3} alt="" />
+              </Link>
+            </li>
+          </ul>
+        </section>
+
+        <section className="contents">
+          <h2 className="a11y-hidden">콘텐츠</h2>
+          <ul>
+            <li>
+              <Link>1:1 코칭</Link>
+            </li>
+            <li>
+              <Link>NEW</Link>
+            </li>
+            <li>
+              <Link>부동산</Link>
+            </li>
+          </ul>
+        </section>
+
+        <section className="classes">
+          <h2>이번 달 BEST 강의</h2>
+          <ul>
+            <li>
+              {/* 루팡 스쿨 기초반 상세로 */}
+              <Link to="/">
+                <img src={best1} alt="루팡 스쿨 기초반" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <img src={best2} alt="루팡 스쿨 주식반" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <img src={best3} alt="루팡 스쿨 중급반" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <img src={best4} alt="서울 투자 중급반" />
+              </Link>
+            </li>
+          </ul>
+        </section>
+      </StyledMain>
     </>
   );
 };
+
+const StyledMain = styled.main`
+  .banners {
+    overflow: hidden;
+    ul {
+      display: flex;
+      height: 400px;
+    }
+    li {
+      flex-shrink: 0;
+      width: 100%;
+    }
+    img {
+      object-fit: cover;
+      /* object-position: 40% top; */
+    }
+  }
+
+  .contents {
+    margin: 50px 0 53px;
+    ul {
+      text-align: center;
+    }
+    li {
+      display: inline-block;
+      width: 92px;
+      padding: 15px 0 13px;
+      font-size: 1.6rem;
+      line-height: 2.3rem;
+      text-align: center;
+      color: var(--black-color);
+      border: 1px solid black;
+    }
+    li + li {
+      margin-left: 20px;
+    }
+    a::before {
+      content: '';
+      display: block;
+      width: 100%;
+      aspect-ratio: 92 / 80;
+      margin-bottom: 18px;
+      border: 1px solid black;
+    }
+  }
+
+  .classes {
+    max-width: 1200px;
+    margin: auto;
+
+    h2 {
+      margin-bottom: 20px;
+      font-size: 3rem;
+      line-height: 4.3rem;
+      font-weight: 700;
+    }
+
+    ul {
+      display: flex;
+      gap: 27px;
+    }
+
+    img {
+      aspect-ratio: 280 / 230;
+    }
+  }
+`;
 
 export default Home;
