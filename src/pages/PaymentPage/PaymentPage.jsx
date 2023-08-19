0c57416835fd061e189cd42688645b833fbb3fbc
yonainthefish <yonaisgood@gmail.com>
2023-08-20 01:10:34 +0900

    desgin: 결제페이지스타일적용

--- src/pages/PaymentPage/PaymentPage.jsx
+++ src/pages/PaymentPage/PaymentPage.jsx
@@ -1,64 +1,88 @@
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-import { PaymentContainor, RightSection } from './PaymentPageStyle';
+import {
+  PaymentContainor,
+  RightSection,
+  LeftSection,
+} from './PaymentPageStyle';
+import BasicClass from '../../assets/images/best/1.png';
 
 const PaymentPage = () => {
   return (
     <>
-      <Header></Header>
+      <Header />
       <PaymentContainor>
-        <h2 className="a11y-hidden">주문결제</h2>
-        <RightSection>
+        <LeftSection>
+          <h2 className="a11y-hidden">주문결제</h2>
           <h1>주문결제</h1>
-          <div>
-            <img src="" alt="" />
-            <div>
+          <div className="className">
+            <img className="classImg" src={BasicClass} alt="" />
+            <div className="classInfo">
               <p>[루팡스쿨 기초반] 월급모아 부자되는 가장 빠른 법</p>
-              <p>월 9,900 원</p>
+              <p className="classPrice">월 9,900 원</p>
             </div>
           </div>
-          <section>
-            <h3>쿠폰</h3>
-            <p>사용가능한 쿠폰이 없습니다.</p>
-            <h3>포인트</h3>
-            <div>
-              <p>0</p>
-              <button>전액사용</button>
+          <section className="payMethod">
+            <div className="payBenefit">
+              <h2 className="a11y-hidden">쿠폰</h2>
+              <h3>쿠폰</h3>
+              <p>사용가능한 쿠폰이 없습니다.</p>
             </div>
-            <h3>결제수단</h3>
-            <div>
-              <button>신용카드</button>
-              <button>A페이</button>
-              <button>B페이</button>
-              <button>C페이</button>
+            <div className="payBenefit">
+              <h2 className="a11y-hidden">포인트</h2>
+              <h3>포인트</h3>
+              <div className="payPoint">
+                <p>0</p>
+                <button className="payAllBtn" type="submit">
+                  전액사용
+                </button>
+              </div>
+            </div>
+            <div className="payBenefit">
+              <h2 className="a11y-hidden">결제수단</h2>
+              <h3>결제수단</h3>
+              <div className="paySelect">
+                <button className="paySelectBtn" type="button">
+                  신용카드
+                </button>
+                <button className="paySelectBtn" type="button">
+                  A페이
+                </button>
+                <button className="paySelectBtn" type="button">
+                  B페이
+                </button>
+                <button className="paySelectBtn" type="button">
+                  C페이
+                </button>
+              </div>
             </div>
           </section>
-        </RightSection>
-        <section>
+        </LeftSection>
+        <RightSection>
           <h3>결제금액</h3>
-          <ul>
+          <ul className="payCost">
             <li>
               <p>총 강의 금액</p>
               <strong>118,800 원</strong>
             </li>
             <li>
               <p>총 강의 금액</p>
-              <strong>118,800 원</strong>
+              <span>118,800 원</span>
             </li>
             <li>
               <p>총 강의 금액</p>
-              <strong>118,800 원</strong>
+              <span>0 원</span>
             </li>
           </ul>
-          <div>
+          <div className="totalCost">
             <p>총 결제금액</p>
-            <strong>118,800 원</strong>
+            <span>118,800 원</span>
           </div>
-          <span>12개월 할부 시 월 9,900원</span>
-        </section>
-        <button>결제하기</button>
+          <span className="installmentInfo">12개월 할부 시 월 9,900원</span>
+          <button>결제하기</button>
+        </RightSection>
       </PaymentContainor>
-      <Footer></Footer>
+      <Footer />
     </>
   );
 };
