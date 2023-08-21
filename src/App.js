4cb33fe4d824b60ce9fbb246be40d2e65f820e7c
yonainthefish <yonaisgood@gmail.com>
2023-08-21 11:06:02 +0900

    design: 페이지 Link 연결

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from './pages/Login';
+import Signup from './pages/SignUp/SignUp';
 import Detail from './pages/DetailPage/DetailPage';
 import Payment from './pages/PaymentPage/PaymentPage';
 import Ranking from './pages/RankingPage/RankingPage';
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" Component={Home}></Route>
           <Route path="/login" Component={Login}></Route>
+          <Route path="/signup" Component={Signup}></Route>
           <Route path="/detail" Component={Detail}></Route>
           <Route path="/payment" Component={Payment}></Route>
           <Route path="/ranking" Component={Ranking}></Route>
