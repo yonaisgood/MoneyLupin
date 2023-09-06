90e776b8973bd48a9a46e37484bed83aeafd811d
yonainthefish <yonaisgood@gmail.com>
2023-09-07 00:30:07 +0900

    Design: 반응형 작업 완료

--- src/pages/PaymentPage/StyledDialog.jsx
+++ src/pages/PaymentPage/StyledDialog.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const StyledDialog = styled.dialog`
-  padding: 31px 25px 25px;
+  padding: 3.1rem 2.5rem 2.5rem;
   border: none;
   border-radius: 20px;
   .name {
@@ -11,9 +11,9 @@ const StyledDialog = styled.dialog`
     color: var(--black-color);
   }
   dl,
-  div {
-    padding: 15px 20px 14px;
-    width: 691px;
+  .box {
+    padding: 1.5rem 2rem 1.4rem;
+    width: 100%;
     border: 1px solid var(--gray-300);
     border-radius: 10px;
     line-height: 2.7rem;
@@ -21,7 +21,7 @@ const StyledDialog = styled.dialog`
 
   dl {
     display: flex;
-    margin: 43px 0 30px;
+    margin: 4rem 0 3rem;
     font-size: 1.4rem;
     color: var(--gray-300);
   }
@@ -32,26 +32,26 @@ const StyledDialog = styled.dialog`
     margin: 0;
   }
   .price {
-    margin-left: 40px;
+    margin-left: 4rem;
   }
   .total {
     margin: 0 0.8rem 0 auto;
   }
   .total,
-  .total + dd {
+  .totalPrice {
     font-size: 2rem;
     font-weight: 700;
     color: var(--error-color);
   }
 
-  div {
+  .box {
     display: flex;
     align-items: center;
     font-size: 1.4rem;
     color: var(--gray-300);
     font-weight: 700;
     span {
-      margin-left: 14px;
+      margin-left: 1.4rem;
     }
   }
 
@@ -90,6 +90,89 @@ const StyledDialog = styled.dialog`
     top: 25px;
     right: 25px;
     font-size: 1.6rem;
+  }
+
+  @media screen and (max-width: 780px) {
+    padding: 2.3rem 1.6rem 1.6rem;
+    width: 52.9rem;
+
+    dl,
+    .box {
+      padding: 1rem 2rem 1.4rem;
+    }
+
+    dl {
+      display: flex;
+      margin: 4rem 0 3rem;
+      font-size: 1.4rem;
+      color: var(--gray-300);
+    }
+
+    dt,
+    dd {
+      font-size: 1.2rem;
+      display: inline-block;
+    }
+
+    .price {
+      margin-left: 2rem;
+    }
+
+    .total,
+    .totalPrice {
+      font-size: 1.5rem;
+      font-weight: 700;
+    }
+
+    p {
+      margin: 1.9rem 0 4.8rem;
+      font-size: 1.2rem;
+    }
+
+    .pay-btn {
+      display: block;
+      margin-left: auto;
+      font-size: 1.6rem;
+    }
+
+    .close-btn {
+      top: 20px;
+      right: 20px;
+      font-size: 1.4rem;
+    }
+  }
+
+  @media screen and (max-width: 430px) {
+    dl {
+      flex-wrap: wrap;
+    }
+
+    dl > div {
+      width: 100%;
+    }
+
+    .price {
+      margin-left: 0rem;
+    }
+
+    .total,
+    .totalPrice {
+      font-size: 1.4rem;
+    }
+
+    .agreeAll {
+      font-size: 1.2rem;
+    }
+
+    .notification {
+      width: 30rem;
+      margin: 2rem auto;
+      line-height: 20px;
+    }
+
+    button {
+      margin: 0 auto;
+    }
   }
 `;
 export default StyledDialog;
